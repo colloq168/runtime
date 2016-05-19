247f8393b7a79e4802fffc49001df90774060fe2
ChrisCaixx <chriscaixx@gmail.com>
2016-05-19 16:45:01 +0800

    创建项目

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by 花菜ChrisCai on 2016/5/19.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime
+//
+//  Created by 花菜ChrisCai on 2016/5/19.
+//  Copyright © 2016年 Chris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
